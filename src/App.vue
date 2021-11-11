48d7724c22a1be45f8a90ac18ad45030b4ef9160
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-11-11 15:07:22 +0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,48 @@
   <div id="app">
     <HomeIcon class="icon1"/>
     <div class="header">
-      <router-link to="/" class="pages mainPage">Головна</router-link>
+      <router-link to="/"><img src="logo.jpg" class="logo"></router-link>
       <router-link to="/MyOffice" class="pages myOffice">Мій кабінет</router-link>
-      <router-link to="/Registration" class="pages myRegistration">Реєстрація</router-link>
-      <router-link to="/AboutE" class="pages mySignIn">Види Єшок</router-link>
+      <router-link to="/Registration" class="pages myRegistration">Реєстрація/Увійти</router-link>
+      <router-link to="/AboutE" class="pages mySignIn">Види добавок</router-link>
       <br>
       <br>
     </div>
     <div class="content">
       <router-view></router-view>
     </div>
-   
+    <div class="footer">
+        <div class='followUs'>
+          <GlobeAltIcon class="icon4"/>
+          <GlobeIcon class="icon5"/>
+      <h3 style="color:#FFFFFF" >FOLLOW US</h3>
+      <p> <a href="">Facebook</a></p>
+      <p> <a href="">Instagram</a></p>
+        </div>
+        <div class='contacts'>
+      <h3 style="color:#FFFFFF">CONTACTS</h3>
+       <PhoneIcon class="icon2"/>
+      <MailIcon class="icon3"/>
+      <p>Call us now: 0123-456-789</p>
+      <p>Email:<a href=""> efood_admin_01@gmail.com</a></p>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import { HomeIcon } from '@iconicicons/vue'
+import { PhoneIcon } from "@vue-hero-icons/solid"
+import { MailIcon } from "@vue-hero-icons/solid"
+import { GlobeAltIcon } from "@vue-hero-icons/outline"
+import { GlobeIcon } from "@vue-hero-icons/outline"
 export default {
   name: 'App',
-  components: {  
+  components: {  HomeIcon,PhoneIcon,MailIcon,GlobeAltIcon,GlobeIcon
   }
 }
 </script>
 
-<style>
-#app{
-font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: Gold	;
-  margin: -8px;
-  }
+<style lang='scss'>
+@import './assets/style';
 </style>
